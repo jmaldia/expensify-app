aaa306162103760fbdb786f1b240196fd9661ee9
Jon Maldia <jon@maldia.net>
2019-03-28 09:45:17 -0400

    Refactor: break up Redux code to multiple files and folders

--- src/app.js
+++ src/app.js
@@ -3,7 +3,28 @@ import ReactDOM from 'react-dom';
 
 import AppRouter from './routers/AppRouter';
 
+// Redux Stuff
+import configureStore from './store/configureStore';
+import { addExpense, removeExpense, editExpense } from './actions/expenses';
+import { setTextFilter, setStartDate, setEndDate,  sortByAmount, sortByDate } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+})
+
+
+store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
