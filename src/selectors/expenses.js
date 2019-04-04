29b92a99a2874ba059a4d8784a4d14ed9c285e71
Jon Maldia <jon@maldia.net>
2019-04-04 14:02:19 -0400

    Add filters test

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,8 +1,12 @@
+import moment from 'moment';
+
 // Get visible expense
 const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
     return expenses.filter((expense) => {
-        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-        const endDateMatch = typeof endDate !== 'number' || expense.createdAt >= endDate;
+        const createdAtMoment = moment(expense.createdAt);
+
+        const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day'): true;
+        const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day'): true;
         // figure out if expense.description as the text variable string inside of it
         // include
         // convert to lower case
