69363b7298c0451062466583a36eae10625a0de7
Jon Maldia <jon@maldia.net>
2019-04-15 16:55:56 -0400

    Refactor EditExpense

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -8,7 +8,7 @@ import ExpenseForm from './ExpenseForm';
 export class AddExpensePage extends React.Component {
     onSubmit = (expense) => {
         // props.dispatch(addExpense(expense));
-        this.props.onSubmit(expense);
+        this.props.addExpense(expense);
         this.props.history.push('/');
     };
 
@@ -26,7 +26,7 @@ export class AddExpensePage extends React.Component {
 } 
 
 const mapDispatchToProps = (dispatch) => ({
-    onSubmit: (expense) =>dispatch(addExpense(expense))
+    addExpense: (expense) =>dispatch(addExpense(expense))
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
